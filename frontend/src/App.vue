97b954e69d5c7290f521f36ee7c0dab5d14b9cac
indraprhmbd <indraprhmbd@gmail.com>
2026-02-14 11:43:45 +0700

    deploy: Nginx integration and production build

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,4 @@
-<script setup>
-import Sidebar from "./components/Sidebar.vue";
-import Header from "./components/Header.vue";
+<script setup lang="ts">
 import ToastContainer from "./components/ToastContainer.vue";
 </script>
 
